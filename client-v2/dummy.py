0312039652816870b25c11ad3f458d60e9433197
Ahaan Dabholkar <ahaandabholkar@gmail.com>
2021-10-25 13:55:20 -0400

    client with working SASL~>SCRAM-SHA-1

--- client-v2/dummy.py
+++ client-v2/dummy.py
@@ -1,5 +1,6 @@
 import socket
 from base64 import b64decode as bd
+from base64 import b64encode as be
 import string
 import secrets
 
@@ -17,7 +18,7 @@ class SCRAM_SHA1:
 		self._n = user
 		self._pwd = pwd
 		self._m = None	# N.S.
-		self._cr = client_nonce or ascii_nonce_generator(length)
+		self._cr = client_nonce or self.ascii_nonce_generator(length)
 		self._sr = server_nonce 
 		self._i = index 
 		self._s = seed
@@ -30,12 +31,16 @@ class SCRAM_SHA1:
 		'''
 		if length is None:
 			length = 5
-		nonce = "".join([secrets.choice() for _ in range(length)])
+		printable_ascii = [i for i in string.printable[:-5] ]
+		nonce = "".join([secrets.choice(printable_ascii) for _ in range(length)])
 		nonce.replace(",", "0")
 		return nonce
 
 	def client_first_message_bare(self):
 		return f"n={self._n},r={self._cr}"
+	
+	def client_first_message(self):
+		return f"{self._gs2_header}{self.client_first_message_bare()}"
 
 	def server_first_message(self):
 		return f"r={self._sr},s={self._s},i={str(self._i)}"
@@ -61,7 +66,9 @@ class SCRAM_SHA1:
 		auth_message = self.client_first_message_bare() + "," + self.server_first_message() + "," + self.client_final_message_without_proof()
 		client_sig = hmac.new(stored_key, auth_message.encode('utf-8'), hashlib.sha1).digest()
 		return self.xor(client_key,client_sig)
-
+		
+	def client_final_message(self):
+		return f'{self.client_final_message_without_proof()},p={be(sasl.generate_client_proof()).decode()}'
 
 if __name__ == '__main__':
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -75,6 +82,19 @@ if __name__ == '__main__':
 		# print(s.recv(2048))
 
 		# SASL Layer
-
-		s.sendall(b'<auth mechanism="SCRAM-SHA-1" xmlns="urn:ietf:params:xml:ns:xmpp-sasl">biwsbj1hZG1pbixyPUM0OTUzODUwREQ1QTM2M0RDMkVENEM1OENENTY4NkY=</auth>')
+		sasl = SCRAM_SHA1(user="admin", pwd="123")
+		# send first message 
+		client_first_msg = f'<auth mechanism="SCRAM-SHA-1" xmlns="urn:ietf:params:xml:ns:xmpp-sasl">{be(sasl.client_first_message().encode()).decode()}</auth>'
+		s.sendall(client_first_msg.encode())
+		# receive first server message
+		import xml.etree.ElementTree as ET
+		first_server_msg = s.recv(2048)
+		resp_root = ET.fromstring(first_server_msg.decode())
+		resp_dict = {k:v for (k,v) in [a.split('=') for a in bd(resp_root.text).decode().split(',')]}
+		sasl._sr, sasl._s, sasl._i = resp_dict['r'], resp_dict['s'], int(resp_dict['i'])
+		client_response = f'<response xmlns="urn:ietf:params:xml:ns:xmpp-sasl">{be(sasl.client_final_message().encode()).decode()}</response>'
+		print(client_response)
+		s.sendall(client_response.encode())
+		
+		# get server response
 		print(s.recv(2048))
